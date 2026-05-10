4177f55f95eacd248e8b40182f27670cca70e94c
FelipeHenrique17 <felipe.h.oliveira2232@gmail.com>
2026-05-10 16:50:33 -0300

    Integração com a Memória

---
+++ produto/produto/src/main/java/fatecfranca/produto/controller/ProdutoController.java
@@ -0,0 +1,59 @@
+package fatecfranca.produto.controller;
+
+import fatecfranca.produto.model.Produto;
+import fatecfranca.produto.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired // injeção de dependência
+    // podemos usar service sem instanciá-lo
+    private ProdutoService service;
+
+    @GetMapping // verbo get
+    public ResponseEntity<List<Produto>> listar(){
+        // status de resposta é ok (sucessoO
+        return ResponseEntity.ok(service.listar());
+    }
+    @PostMapping
+    public ResponseEntity<Produto> criar(@RequestBody Produto p){
+        // chama o garçom
+        Produto criado = service.criar(p);
+        // cria uma uri com o id do produto criado
+        // URL - Uniform Resource Locator
+        // URI - Uniform Resource Identifier
+        URI uri = URI.create("/produtos/" + criado.getId());
+        // retorna status de sucesso 201 (created)
+        return ResponseEntity.created(uri).body(criado);
+    }
+
+    @DeleteMapping ("/{id}")
+    public ResponseEntity<Void> remover(@PathVariable long id) {
+        return service.remover(id) ?
+                ResponseEntity.noContent().build():
+                ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping ("/{id}")
+    public ResponseEntity<Produto> atualizar(
+            @PathVariable Long id,
+            @RequestBody Produto produto
+            ){
+            Produto ret = service.atualizar(id, produto);
+            if (ret == null){
+                // não encontrou para atualizar
+                return ResponseEntity.notFound().build();
+            }
+            else {
+                // retorna produto atualizado
+                return ResponseEntity.ok(ret);
+        }
+    }
+}
---
+++ produto/produto/src/main/java/fatecfranca/produto/service/ProdutoService.java
@@ -0,0 +1,45 @@
+package fatecfranca.produto.service;
+
+import fatecfranca.produto.model.Produto;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    //criar a lista de produtos
+    private List<Produto> lista = new ArrayList<Produto>();
+    private Long nextId = 1L;
+
+    public List<Produto> listar(){
+        return this.lista;
+    }
+
+    public Produto criar(Produto p){
+        p.setId(nextId); // insere id
+        nextId++; // atualiza id
+        this.lista.add(p); // adiciona na lista
+        return p; // retorna produto
+    }
+
+    public boolean remover(Long id){
+        for (int i=0;i<this.lista.size();i++){
+            if (lista.get(i).getId().equals(id)){
+                this.lista.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+    public Produto atualizar(Long id, Produto novo){
+        for(int i=0;i<lista.size();i++){
+            if (lista.get(i).getId().equals(id)){
+                novo.setId(novo.getId());
+                lista.set(i, novo);
+                return novo; //atualizou
+            }
+        }
+        return null; //não atualizou
+    }
+}
